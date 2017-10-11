602711966bce064b09a99cbeec18d233db1b0bec
koostya <dolgih.konstantinn@gmail.com>
2017-10-12 01:53:35 +0300

    added blocks {news, about, our brands, consultation}

--- dev/js/index.js
+++ dev/js/index.js
@@ -6,6 +6,8 @@ require('../scss/style.scss');
 
 // vendors
 require('./slick/slick.min.js');
+require('./slick/my_slick.js');
+
 // my
 require('./tabs/index.js');
 
---
+++ dev/js/slick/my_slick.js
@@ -0,0 +1,9 @@
+$('.brands_carusel').slick({
+  slidesToShow: 5,
+  swipeToSLide: true,
+  centerMode: true,
+  variableWidth: true,
+  prevArrow: '<div class="slick-prev"></div>',
+  nextArrow: '<div class="slick-next"></div>',
+  arrows: true
+});
